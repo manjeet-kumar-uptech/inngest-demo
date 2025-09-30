1ce5c786f251a0e9a47c93c8f92e98db5203abf7
manjeet-kumar-uptech <manjeetuptech@gmail.com>
2025-09-30 12:19:58 +0530

    added inngest signing key

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -3,4 +3,5 @@ import { Inngest } from "inngest";
 export const inngest = new Inngest({
   id: "inngest-demo-app",  // unique app identifier
   eventKey: process.env.INNGEST_EVENT_KEY,
+  apiKey: process.env.INNGEST_SIGNING_KEY,
 });
